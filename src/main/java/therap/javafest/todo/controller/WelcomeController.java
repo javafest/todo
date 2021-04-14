66aae6086fedfbcd10123067c4d81c03bc28e20d
Erfan Ahmed <erfan@therapservices.net>
2021-04-14 13:50:06 +0600

    JSP configuration  * Tips: tomcat-embed-jasper dependency is required for jsp

--- src/main/java/therap/javafest/todo/controller/WelcomeController.java
+++ src/main/java/therap/javafest/todo/controller/WelcomeController.java
@@ -1,17 +1,17 @@
 package therap.javafest.todo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author erfan
  * @since 4/14/21
  */
-@RestController
+@Controller
 public class WelcomeController {
 
     @GetMapping
     public String greeting() {
-        return "Hello from Javafest";
+        return "welcome";
     }
 }
---
+++ src/main/webapp/WEB-INF/welcome.jsp
@@ -0,0 +1,13 @@
+<%--
+  User: erfan
+  Date: 4/14/21
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h2>Welcome to Therap Javafest</h2>
+</body>
+</html>
